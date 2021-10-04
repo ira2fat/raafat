8cb16c1f2e97667a1f6ddbb498b21ab19181db6b
raafat <mra2fat@gmail.com>
2021-10-04 20:41:29 +0200

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+export default function App(){
+const raafat =(event)=>{
+  console.log(event.value)
+  alert("Hello",event.target.value)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
+  return (
+  <><h1>Mohamed Raafat</h1><form onSubmit={raafat}>
 
-export default App;
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form></>
+    )
+} 
