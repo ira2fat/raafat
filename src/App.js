23c06578f0905f10db9b88f07c4b78761658371b
raafat <mra2fat@gmail.com>
2021-10-04 21:19:30 +0200

    hi there

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from "react";
 export default function App(){
-const raafat =(event)=>{
-  console.log(event.value)
-  alert("Hello",event.target.value)
+  const [name,setName]=React.useState("")
+const raafat =()=>{
+alert(`hello ${name}`);
+ 
+};
+
+
+const Submit =(event)=>{
+  setName (event.target.value);
 
 }
+
   return (
-  <><h1>Mohamed Raafat</h1><form onSubmit={raafat}>
+  <><h1>Greetings</h1><form onSubmit={raafat}>
 
       <label>
         Name:
-        <input type="text" name="name" />
+        <input onChange={Submit} type="text" name="name" defaultValue={name}/>
       </label>
       <input type="submit" value="Submit" />
-    </form></>
+    </form> 
+    <br/>
+  
+    <img src="https://media.istockphoto.com/photos/social-network-concept-picture-id1281414200?s=612x612"/>
+    </>
     )
 } 
