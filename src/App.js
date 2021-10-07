ed7640c95282ae3ea91900b1d6707a6c795caefc
raafat <mra2fat@gmail.com>
2021-10-07 09:38:40 +0200

    new updates to components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Counter from "./components/counter";
 import Header from "./components/header";
+import Video from "./video";
 
 export default function App() {
 
   return (
     <><Header link="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGlly-WC6mU0q6iszakSM6hYFJR4RPhzUxET_WMTmSyFfKDuUnkhvVdWse7zvsjPIWxa0&usqp=CAU" />
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/jofNR_WkoCE" title="YouTube video player" frameborder="0" style={{ "justify-content": "center" }} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <Video/>
+
         <Counter />
       </div>
         <footer style={{ position: "fixed", bottom: "0px" }}>
-        Copyright &#169; Mohamed ELdakar
+        Copyright &#169; Bosch.IO
       </footer></>
 
   )
---
+++ src/video.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Video(){
+return(
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/jofNR_WkoCE" title="YouTube video player" frameborder="0" style={{ "justify-content": "center" }} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+)
+}
+export default  Video
+
+
